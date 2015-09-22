1e05cd659d6cfa57a64fda67fa020e8c4a4c144f
Korben Zhang <korbenzhang@gmail.com>
2015-09-22 19:12:50 +0800

    fix test

--- i18n_test.go
+++ i18n_test.go
@@ -1,32 +1,28 @@
 package mmsg
 
 import (
-	"testing"
 	. "github.com/mabetle/mcore/mtest"
+	"testing"
 )
 
 func TestPutMsg(t *testing.T) {
-	PutMsg("en","hello", "hello demo")
-	AssertEqual(LocaleMessage("en","hello"), "hello demo")
-	//default to zh, but i donot set zh key
-	AssertEqual(Message("hello"), "hello demo")
+	PutMsg("en", "hello", "hello demo")
+	AssertEqual(Message("en", "hello"), "hello demo")
 }
 
-func TestPutText(t *testing.T){
-	text:=`
+func TestPutText(t *testing.T) {
+	text := `
 # this is a demo
 hello2=hello demo
 name=demo
 	`
 	PutMsgText("en", text)
-	AssertEqual(Message("hello2"), "hello demo")
-	AssertEqual(Message("name"), "demo")
+	AssertEqual(Message("en", "hello2"), "hello demo")
+	AssertEqual(Message("en", "name"), "demo")
 }
-
 
 func TestFile(t *testing.T) {
 	LoadMessages("testdata")
-	AssertEqual(LocaleMessage("en", "hello"),"Hello from English")
-	AssertEqual(LocaleMessage("zh", "hello"),"Hello from Chinese, 你好")
+	AssertEqual(Message("en", "hello"), "Hello from English")
+	AssertEqual(Message("zh", "hello"), "Hello from Chinese, 你好")
 }
-
